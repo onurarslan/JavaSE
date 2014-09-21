d0c78e3c53abcefcebeeeeb2cb205e2bdf248294
onur.arslan <onurrarslan@hotmail.com>
2014-09-21 16:27:11 +0300

    chapter1-identifiers bölüm1-tanımlamalar

--- JavaSE/src/Chapter1_Identifiers_Tanimlamalar.java
+++
@@ -1,9 +0,0 @@
-
-public class Chapter1_Identifiers_Tanimlamalar {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
---
+++ JavaSE/src/OCJP/Chapter1_Identifiers_Tanimlamalar.java
@@ -0,0 +1,22 @@
+package OCJP;
+public class Chapter1_Identifiers_Tanimlamalar {
+
+	public static void main(String[] args) {
+		//legal identifiers. kurallara uygun tanımlamalar
+		int number=1;
+		int number2=2;
+		int _number3=3;
+		int $number4=4;
+		
+		System.out.println(number+number2+_number3+$number4);
+		
+		/*illegal identifiers. kurallara uygun olmayan tanımlamalar
+		 * int 1number=1;
+		 * int :number2=2;
+		 * int #number3=3;
+		 * int .number4=4;
+		 * int +number5=5;
+		 * int -number6=6; 
+		 */
+	}
+}
